dff7599b7922ba1d024366d68a4ea79352dfaee4
REX_HOLMSTROM <rexholmstrom96@gmail.com>
2020-08-08 15:30:39 +0200

    api

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ window.addEventListener('load', () => {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude
+
+            const api = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&long={long}&appid={bb1832fb10b414bf45d8951cf0c984ee}`
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data)
+                })
         });
-    } else {
-        h1.textContent = "hey this is not working"
     }
 })
